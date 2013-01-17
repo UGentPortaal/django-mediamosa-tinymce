c51cba83f45da0c7fe5d0a151bbaf87f1595a14a
Kevin Van Wilder <kevin@inuits.eu>
2013-01-17 16:23:27 +0100

    Fixed assetlistdialog to only show assets with streamable mediafiles

--- mediamosa_tinymce/views.py
+++ mediamosa_tinymce/views.py
@@ -1,4 +1,7 @@
-from django.views.generic import ListView
+import json
+
+from django.views.generic import ListView, DetailView
+from django.http import HttpResponse, Http404
 
 from django_mediamosa.base import api
 
@@ -8,5 +11,23 @@ class TinyMceAssetSelectionDialog(ListView):
     paginate_by = 10
 
     def get_queryset(self):
-        asset_list = api.asset_list()
+        asset_list = api.asset_list(mime_type='mp4', mime_type_match='contains')
         return asset_list
+
+
+class JSONTinyMceMediafileId(DetailView):
+
+    def get_object(self, queryset=None):
+        asset_id = self.request.GET.get('id')
+        try:
+            mediafile = api.asset(asset_id).get_mediafile()
+        except:
+            return None
+        return mediafile
+
+    def render_to_response(self, context, **response_kwargs):
+        if self.object is None:
+            raise Http404
+        else:
+            return HttpResponse(
+                json.dumps({'mediafile_id': self.object.id}))
