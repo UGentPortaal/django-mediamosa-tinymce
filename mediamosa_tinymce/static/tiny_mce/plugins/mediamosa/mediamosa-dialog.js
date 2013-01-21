907c1ac0321f840370b0ebe2071080c8b1d77855
Kevin Van Wilder <kevin@inuits.eu>
2013-01-21 12:42:25 +0100

    Minimized api queries by only querying for mediafile id when inserting asset.

--- mediamosa_tinymce/static/tiny_mce/plugins/mediamosa/mediamosa-dialog.js
+++ mediamosa_tinymce/static/tiny_mce/plugins/mediamosa/mediamosa-dialog.js
@@ -1,23 +1,40 @@
 tinyMCEPopup.requireLangPack();
 
 var MediaMosaDialog = {
-    preInit : function() {},
+    preInit : function() {
+
+    },
 
     init : function() {
 
     },
 
-    resize : function() {},
+    resize : function() {
+
+    },
 
     insert : function(data) {
 
-        tinyMCEPopup.execCommand('mceMediaMosaInsert', false, {
-            asset_id: data.asset_id,
-            mediafile_id: data.mediafile_id,
-            still_url: data.url
+        // fetch mediafile id
+        $.ajax({
+            url: MediafileIdQueryURI +"?id=" + data.asset_id
+        }).done(function(jsondata) {
+
+            // parse as json
+            jsondata = jQuery.parseJSON( jsondata );
+
+            // insert into tinymce
+            tinyMCEPopup.execCommand('mceMediaMosaInsert', false, {
+                asset_id: data.asset_id,
+                mediafile_id: jsondata.mediafile_id,
+                still_url: data.url
+            });
+
+            // close the dialog
+            tinyMCEPopup.close();
+
         });
 
-        tinyMCEPopup.close();
     }
 };
 
--- mediamosa_tinymce/views.py
+++ mediamosa_tinymce/views.py
@@ -1,4 +1,7 @@
-from django.views.generic import ListView
+import json
+
+from django.views.generic import ListView, DetailView
+from django.http import HttpResponse, Http404
 
 from django_mediamosa.base import api
 
@@ -9,4 +12,22 @@ class TinyMceAssetSelectionDialog(ListView):
 
     def get_queryset(self):
         asset_list = api.asset_list()
-        return asset_list
+        return filter(lambda asset: asset.has_streamable_mediafiles, asset_list)
+
+
+class JSONTinyMceMediafileId(DetailView):
+
+    def get_object(self, queryset=None):
+        asset_id = self.request.GET.get('id')
+        try:
+            mediafile = api.asset(asset_id).get_mediafile()
+        except:
+            return None
+        return mediafile
+
+    def render_to_response(self, context, **response_kwargs):
+        if self.object is None:
+            raise Http404
+        else:
+            return HttpResponse(
+                json.dumps({'mediafile_id': self.object.id}))
